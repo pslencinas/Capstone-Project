49e96a997d5dea5e2448f993f2d830e89ddc6359
pslencinas <pslencinas@gmail.com>
2016-07-20 14:04:06 -0300

    adding json food db and SQLIte Content Provider

--- app/src/main/java/com/example/android/myargmenuplanner/MainActivity.java
+++ app/src/main/java/com/example/android/myargmenuplanner/MainActivity.java
@@ -81,17 +81,13 @@ public class MainActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_ingredients) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_foods) {
 
-        } else if (id == R.id.nav_slideshow) {
+        } else if (id == R.id.nav_search) {
 
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
+        } else if (id == R.id.nav_option) {
 
         }
 
